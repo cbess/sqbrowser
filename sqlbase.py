908cfd1ffe138c3e7e6cbb20311ece78f4d824ab
C. Bess <cbess@quantumquinn.com>
2013-04-26 22:29:50 -0500

    - more python in python-only module

--- sqlbase.py
+++ sqlbase.py
@@ -15,6 +15,7 @@ try:
 except ImportError:
     from pysqlite2 import dbapi2 as sqlite3
 
+
 class SqlBase:
     def __init__(self, dbpath):
         """__init__
@@ -23,18 +24,16 @@ class SqlBase:
         self.cursor = None
         self.db_path = dbpath
         self.is_connected = False
-        self.last_error = ""
-        pass
-        
+        self.last_error = ''
+
     def connect(self):
         """connect
         """
         if self.is_connected:
             return True
-            
+
         # check for existance
         if not os.path.isfile(self.db_path):
-            # raise Exception("Check the DB path.")
             return False
 
         ## connect to the db
@@ -43,43 +42,42 @@ class SqlBase:
         self.cursor = self.db.cursor()
         self.is_connected = True
         return self.is_connected
-        
+
     def execute_query(self, query):
         """Executes the specified query
         @return Tuple of results, upon success or
         None, if error (check self.last_error)
         """
         self.connect()
-        results = None
-        
+
         ## get the results
-        
+
         try:
             results = self.cursor.execute(query)
         except Exception, e:
             self.last_error = str(e)
             return None
-        
+
         # set default query result, used it non-select query executed
-        queryResult = { 
-            "columns" : ['?'],
-            "rows" : (['No rows returned'],),
-            "message" : ""
+        queryResult = {
+            'columns': ['?'],
+            'rows': (['No rows returned'],),
+            'message': ''
         }
-        
+
         # get the rows
-        values = results.fetchall()  
+        values = results.fetchall()
         if values:
             # get the     columns names
             cols = [r[0] for r in results.description]
-        
+
             queryResult = {
-                "columns" : cols,
-                "rows" : values,
-                "message" : "--> Rows returned: %d" % len(values)
+                'columns': cols,
+                'rows': values,
+                'message': '--> Rows returned: %d' % len(values)
             }
         return queryResult
-        
+
     def disconnect(self):
         """disconnect
         """
@@ -89,13 +87,11 @@ class SqlBase:
         self.cursor.close()
         self.db.close()
         self.is_connected = False
-        pass
-        
+
     def commit(self):
         """Commits the query results
         """
         self.db.commit()
-        pass
 
 if __name__ == '__main__':
     main()
